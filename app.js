98bc02965a551ee1b5d5ec6c1cb0db7646235b69
Marco <4high20@gmail.com>
2018-09-10 22:39:30 +0200

    add basics pug views

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send('<h1>I love Treehouse!</h1>');
+  res.render('index');
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<h1>Hello World</h1>');
+app.get('/cards', (req, res) => {
+  res.render('cards', {prompt: "Who is buried in Grant's tomb?"});
 });
 
 app.listen(3000, () => {
